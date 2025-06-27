10e254335a87934d09918daad8c9644392482a0d
Pushkar Cherukuri <pushkarcherukuri@gmail.com>
2025-06-27 11:54:16 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -82,7 +82,7 @@ IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
 GO
 
 CREATE TABLE bronze.erp_px_cat_g1v2 (
-    id           NVARCHAR(50),
+    cid           NVARCHAR(50),
     cat          NVARCHAR(50),
     subcat       NVARCHAR(50),
     maintenance  NVARCHAR(50)
